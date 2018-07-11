81b6e4b992613f60391f72b1b5ea44c2de0b1c48
Yan Nikolaev <yannikolaev@gmail.com>
2018-07-11 20:24:40 +0100

    2 deploy

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const forceSSL = function () {
 app.use(forceSSL());
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + 'PurpleBoard/dist'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
