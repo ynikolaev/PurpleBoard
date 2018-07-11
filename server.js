5ed2dec277e4e4c5c73eb92e442d7f420a432ad6
Yan Nikolaev <yannikolaev@gmail.com>
2018-07-11 21:38:17 +0100

    0.0.5
    Solution to unexpected tokken in dist

--- server.js
+++ server.js
@@ -1,33 +1,40 @@
 const express = require('express');
-const app = express();
 const path = require('path');
-// If an incoming request uses
-// a protocol other than HTTPS,
-// redirect that request to the
-// same url but with HTTPS
-const forceSSL = function () {
-    return function (req, res, next) {
-        if (req.headers['x-forwarded-proto'] !== 'https') {
-            return res.redirect(
-                ['https://', req.get('Host'), req.url].join('')
-            );
-        }
-        next();
-    }
-}
-// Instruct the app
-// to use the forceSSL
-// middleware
-app.use(forceSSL());
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + 'PurpleBoard/dist'));
-// Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 8080);
+const http = require('http');
+const bodyParser = require('body-parser');
 
-// For all GET requests, send back index.html
-// so that PathLocationStrategy can be used
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+// Get our API routes
+const api = require('./server/routes/api');
+
+const app = express();
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Set our api routes
+app.use('/api', api);
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on localhost:${port}`));
--- src/app/routes/carousel/carousel.component.ts
+++ src/app/routes/carousel/carousel.component.ts
@@ -13,8 +13,8 @@ export class CarouselComponent implements OnInit {
     background2;
     @ViewChild('carousel') carousel: any;
     constructor(config: NgbCarouselConfig) {
-        this.background1 = this.images.push("/assets/images/background1.gif");
-        this.background2 = this.images.push("/assets/images/background1.png");
+        this.background1 = this.images.push("./assets/images/background1.gif");
+        this.background2 = this.images.push("./assets/images/background1.png");
         // customize default values of carousels used by this component tree
         config.interval = 0;
         config.wrap = false;
